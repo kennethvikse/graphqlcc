180521297229df097206c266f7b71719eaa592ca
Kenneth <94906413+kenedy69@users.noreply.github.com>
2022-11-11 10:00:25 +0100

    Changes to be committed: 	new file:   GraphQL Queries & Mutations 	modified:   server/schema/schema.js

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -82,6 +82,7 @@ const RootQuery = new GraphQLObjectType({
 const mutation = new GraphQLObjectType({
       name: 'Mutation',
       fields: {
+            // Add a client
             addClient: {
                   type:ClientType,
                   args: {
@@ -95,7 +96,18 @@ const mutation = new GraphQLObjectType({
                               email: args.email,
                               phone: args.phone,
                         });
+                        
                         return client.save();
+                  },
+            },
+            // Delete a client
+            deleteClient: {
+                  type: ClientType,
+                  args: {
+                        id: { type: GraphQLNonNull(GraphQLID) },
+                  },
+                  resolve(parent, args) {
+                        return Client.findByIdAndRemove(args.id);
                   },
             },
       },
